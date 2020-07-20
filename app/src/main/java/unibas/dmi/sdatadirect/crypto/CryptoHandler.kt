0f5308db2e6c14bde38340dd1616820720fdc49a
GowthamanaG <gowthaman.gobalasingam@gmail.com>
2020-07-20 18:01:24 +0200

    Added CryptoHandler class, using AES and RSA

--- app/src/main/java/unibas/dmi/sdatadirect/crypto/CryptoHandler.kt
+++ app/src/main/java/unibas/dmi/sdatadirect/crypto/CryptoHandler.kt
@@ -1,4 +1,38 @@
 package unibas.dmi.sdatadirect.crypto
 
+import java.io.File
+import java.io.FileInputStream
+import java.security.Key
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+
 class CryptoHandler {
+
+    var kgAES: KeyGenerator
+    lateinit var kgRSA: KeyGenerator
+    var secKey: SecretKey
+    lateinit var cipherAES: Cipher
+
+    init {
+        kgAES = KeyGenerator.getInstance("AES")
+        kgAES.init(128)
+        secKey = kgAES.generateKey()
+        cipherAES = Cipher.getInstance("AES")
+    }
+
+    fun encrypt(filePath: String, key: Key) {
+        val fileInputStream = FileInputStream(File(filePath))
+        val fileToByteArray: ByteArray = fileInputStream.readBytes()
+
+        cipherAES.init(Cipher.ENCRYPT_MODE, key)
+
+        val encryptedFile: ByteArray = cipherAES.doFinal(fileToByteArray)
+    }
+
+    fun decrypt(encryptedFile: ByteArray, key: Key) {
+
+    }
+
+
 }
