ffeb449e9feda9db76b3d293162fd6712fb7a3de
Gowthaman Gobalasingam <gowthaman.gobalasingam@gmail.com>
2020-07-26 15:30:55 +0200

    Bluetooth adapter for UI

--- app/src/main/java/unibas/dmi/sdatadirect/ui/BluetoothDeviceListAdapter.kt
+++ app/src/main/java/unibas/dmi/sdatadirect/ui/BluetoothDeviceListAdapter.kt
@@ -1,4 +1,32 @@
 package unibas.dmi.sdatadirect.ui
 
-class BluetoothDeviceListAdapter {
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ListAdapter
+import android.widget.TextView
+import unibas.dmi.sdatadirect.R
+
+class BluetoothDeviceListAdapter(context: Context, val resource: Int, val devices: ArrayList<BluetoothDevice?>):
+    ArrayAdapter<BluetoothDevice>(context, resource, devices) {
+
+    private val layoutInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val tempConvertView = layoutInflater.inflate(resource, parent, false)
+        val device: BluetoothDevice? = devices[position]
+
+        val deviceName: TextView = tempConvertView.findViewById(R.id.deviceName)
+        val deviceAddress: TextView = tempConvertView.findViewById(R.id.deviceAddress)
+
+        deviceName.text = device?.name
+        deviceAddress.text = device?.address
+
+        return tempConvertView
+    }
 }
